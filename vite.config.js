db8b7985a360f0606d57999480aa26c2e77a161a
Harold Sikkema <harold@nsitu.ca>
2025-12-21 13:00:54 -0500

    fix: Update  base path usage in vite config for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 
-// The repo name is always the bit after the "/" 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+// NOTE: this repo is hosted on rivvon.ca
+// and as such the base path can remain at the default value "/"
+// this differs from other github pages repos where the base path
+// needs to be set to "/repository-name/"
 
 export default defineConfig({
-    // Locally you still get "/", but on Actions you get "/repositoryName/"
-    base: process.env.GITHUB_ACTIONS === 'true' ? `/${repositoryName}/` : '/',
+
     build: {
         target: 'esnext', // Enable top-level await and modern JS features
     },
